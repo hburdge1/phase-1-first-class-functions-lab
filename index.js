5036aed73ba6174ee8955e32e4a289b1f5f15792
Hannah Burdge <hburdge1@gmail.com>
2021-12-13 21:07:23 -0800

    commit tree 5ea11e2dbdf811ab0759e10277eb48ecf10bda0a

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (function(drivers) {
+    return [drivers[0], drivers[1]] ;
+}) ;
+
+const returnLastTwoDrivers = (function(drivers){
+    return ['Amari', 'Mo'];
+
+});
+
+var selectingDrivers = ([returnFirstTwoDrivers + returnLastTwoDrivers])
+
+let num
+function createFareMultiplier(num) {
+    return num * fare ;
+}
+
+function fareDoubler(fare) {
+    const doubled = createFareMultiplier(2) 
+        return doubled * (fare)
+}
